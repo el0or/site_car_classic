b428a9246323d9f2a91d26553f709b6b4df25539
el0or <igor050406@mail.ru>
2024-06-01 11:06:45 +0300

    add Heading.jsx and some styles

--- main/src/App.jsx
+++ main/src/App.jsx
@@ -1,10 +1,12 @@
 import './App.css'
 import Header from './components/Header'
+import Heading from './components/Heading'
 
 export default function App() {
   return (
     <div className='wrapper'>
       <Header/>
+      <Heading/>
     </div>
   )
 }
---
+++ main/src/components/Heading.jsx
@@ -0,0 +1,9 @@
+export default function Heading() {
+    return(
+        <div className="heading">
+            <h1>Retro cars 1960-1970</h1>
+            <p>Vintage retro <br /> cars USA</p>
+            <button>Find a car </button>
+        </div>
+    )
+}
