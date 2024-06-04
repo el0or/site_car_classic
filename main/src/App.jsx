7e726fa1011deedec9e9932bb9a1737eab09ac6e
el0or <igor050406@mail.ru>
2024-06-04 20:19:39 +0300

    update item, app, index.css

--- main/src/App.jsx
+++ main/src/App.jsx
@@ -10,24 +10,40 @@ const itemsList = [
     title: '1957 Chevrolet Corvette',
     img: '/img/corvette.png',
     price: '$39 000',
+    button: '14,900 miles',
   },
   {
     id: 2,
     title: '1960 Toyota 2000GT',
     img: '/img/toyota.png',
     price: '$66 700',
+    button: '14,900 miles',
   },
   {
     id: 3,
     title: '1969 Dodge Charger',
     img: '/img/charger.png',
     price: '$39 000',
+    button: '14,900 miles',
   },
   {
     id: 4,
     title: '1967 Toyota 2000GT',
     img: '/img/daytona.png',
     price: '$66 700',
+    button: '14,900 miles',
+  },
+  {
+    id: 5,
+    title: '1967 Toyota 2000GT',
+    img: '/img/pontiac.png',
+    price: '$66 700',
+    button: '14,900 miles',
+  },
+  {
+    id: 6,
+    img: '/img/more_car.png',
+    button: '29 more cars',
   },
 ];
 
--- main/src/components/Item.jsx
+++ main/src/components/Item.jsx
@@ -1,9 +1,11 @@
 export default function Item({ ...props }) {
     return(
-        <div className="item">
-              <img src={props.item.img}/>
-              <p>{props.item.price}</p>
-              <h1>{props.item.title}</h1>
+        <div className="item" style={{ background: props.item.id === 6 ? '#3A3B35' : 'inherit' }}>
+            <img src={props.item.img}/>
+            <p>{props.item.price}</p>
+            <h1>{props.item.title}</h1>
+            <button className="rent">Rent</button>
+            <button>{props.item.button}</button>
         </div>
     )
 }
